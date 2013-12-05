80ab95ba6bb4001d4b65a6d84f47eb6d676d85c5
Julien Fleury <julien.fleury@ensimag.fr>
2013-12-05 19:42:48 +0100

    models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 TheDuckKiller9::Application.routes.draw do
+  resources :associations
+
+  resources :gifts
+
+  resources :ducks
+  
+  root 'ducks#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

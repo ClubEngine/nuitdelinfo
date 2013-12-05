743f58ab3662246f77939384aeca9e90c5cf2a74
Julien Fleury <julien.fleury@ensimag.fr>
2013-12-05 18:18:11 +0100

    add

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TheDuckKiller9::Application.config.secret_key_base = '3711c665d4c9b4127e5a0a3a7a30b994c041b8f26013245f77d24f64949ce0b75e4b5eec61f911acf517aea1e769c425f0f660d415b653de162360cec547bd43'

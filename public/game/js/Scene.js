e820c63da02df3341eb6116e293838b8d632d85e
ArthurSonzogni <Arthur.Sonzogni@ensimag.fr>
2013-12-05 22:39:20 +0100

    ajout

--- public/game/js/Scene.js
+++ public/game/js/Scene.js
@@ -11,7 +11,7 @@ define(
 
 		Scene.prototype.addDuck = function (duckName) {
 			var self = this;
-			this.loader.load('http://localhost/game/assets/jsobj/' + duckName + '.js', function (geometry, materials) {
+			this.loader.load('./assets/jsobj/' + duckName + '.js', function (geometry, materials) {
 				material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
 				var duck = new Duck(geometry, material);
 				self.add(duck);

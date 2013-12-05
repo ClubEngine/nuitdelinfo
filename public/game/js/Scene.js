370298ef328c04b6de85ee9f1592456ea16d68ee
ArthurSonzogni <Arthur.Sonzogni@ensimag.fr>
2013-12-05 21:20:19 +0100

    ajout

--- public/game/js/Scene.js
+++ public/game/js/Scene.js
@@ -1,22 +1,32 @@
 define(
-	[],
-	function () {
-		var Scene = function () {
+	["Duck","jquery","three"],
+	function (duck,$,THREE) {
+		var scene = function () {
 			// constructor
+			var ducks = new array()
 		}
 
-		Scene.prototype = {
+		scene.prototype = {
 			init: function () {
+				ducks.push(new duck())
 
-			},
+				this.camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+				this.camera.position.z = 1000;
+				this.scene = new THREE.Scene();
+				this.renderer = new THREE.CanvasRenderer();
+				this.renderer.setSize( window.innerWidth, window.innerHeight );
+
+				document.body.appendChild( renderer.domElement );
+					},
 
 			animate: function () {
-				requestAnimationFrame($.proxy(this.animate, this));
+				requestanimationframe($.proxy(this.animate, this));
 				console.log('animate');
+				this.renderer.render( this.scene, this.camera );
 				// method
 			}
 		}
 
-		return Scene;
+		return scene;
 	}
 );
